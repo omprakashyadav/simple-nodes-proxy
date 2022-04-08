5a741e222a2cc431f4381e46cbba1d46a9be778d
omprakashyadav <om@evolphin.com>
2022-04-07 20:49:44 -0700

    Push

---
+++ index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const morgan = require("morgan");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+// Create Express Server
+const app = express();
+
+// Configuration 
+const PORT = 3000;
+const HOST = "localhost";
+const SERVICE_URL = process.argv[2];
+
+// Logging
+app.use(morgan('dev'));
+
+// Info GET endpoint
+app.get('/info', (req, res, next) => {
+    res.send('This is a proxy service which proxies to JSONPlaceholder API.');
+});
+
+
+// Proxy endpoints
+app.use('/', createProxyMiddleware({
+    target: SERVICE_URL,
+    changeOrigin: true,
+    hostRewrite:true,
+    pathRewrite: {
+        [`^/`]: '',
+    },
+}));
+
+// Start Proxy
+app.listen(PORT, HOST, () => {
+    console.log(`Starting Proxy at ${HOST}:${PORT}`);
+});
